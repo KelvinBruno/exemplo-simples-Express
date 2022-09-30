c0cef005dd8ca9a2f0d1d15134f6c75636fab9cf
KelvinBruno <kelvinbrunolopes@gmail.com>
2022-09-29 21:29:47 -0300

    feat:get

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (request, response) => {
+  response.send("ADICIONE UM TEXTO");
+});
+app.listen(port, () => {
+  console.log(`App rodando na porta ${port}`);
+});
+app.get("/ferrari", (request, response) => {
+  response.send("Ferrari");
+});
+app.get("/", (request, response) => {
+  response.send("Ferrari");
+});
+
+app.get("/carros", (request, response) => {
+  // seu código aqui
+  response.send(["Ford", "Ferrari", "Chevrolet"]);
+});
